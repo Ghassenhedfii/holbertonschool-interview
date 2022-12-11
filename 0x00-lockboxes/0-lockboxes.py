37cf9cf72b9ad1b8a117431ca389e652852e86b6
Ghassenhedfii <3681@holbertonstudents.com>
2022-12-11 23:26:05 +0100

    Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lockboxes."""
+
+def canUnlockAll(boxes):
+    """There can be keys that do not have boxes."""
+    keys = [0]
+
+    for key in keys:
+        for i in boxes[key]:
+            if i not in keys and i < len(boxes):
+                keys.append(i)
+    for i in range(len(boxes)):
+        if i not in keys:
+            return False
+    return True
