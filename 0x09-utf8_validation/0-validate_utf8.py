21567c5243226c0b80a609221dca4be1e62fd151
Ghassenhedfii <3681@holbertonstudents.com>
2023-02-16 10:35:31 +0100

    validate utf8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+determines if a given data set represents a valid 
+UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    i = 0
+    while i < len(data):
+        count = 0
+        mask = 1 << 7
+        while mask & data[i]:
+            count += 1
+            mask >>= 1
+        if count == 0:
+            pass
+        elif count == 1 or count > 4:
+            return False
+        else:
+            for j in range(i + 1, i + count):
+                if j >= len(data) or (data[j] >> 6) != 2:
+                    return False
+            i += count - 1
+        i += 1
+    return True
