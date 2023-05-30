76911da5ea170ab16b5c999d34c5ed94c56bd2e0
Ghassenhedfii <3681@holbertonstudents.com>
2023-05-30 11:58:50 +0100

    starWars

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+
+const Args = process.argv.slice(2);
+const Id = Args[0];
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${Id}/`,
+  json: true,
+  transform (body, response) {
+    if (response.headers['content-type'] === 'application/json') {
+      response.body = JSON.parse(body);
+    }
+    return response;
+  }
+};
+
+request(options, async (error, response, body) => {
+  if (error) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+  for (const characterURL of response.body.characters) {
+    await new Promise((resolve, reject) => {
+      request(characterURL, (err, res, body1) => {
+        if (err) {
+          reject(new Error(`HTTP error: ${res.status}`));
+        }
+        console.log(JSON.parse(body1).name);
+        resolve();
+      });
+    });
+  }
+});
