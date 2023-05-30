4cfb2cc6595cdc9eb20a1f50e8b1d361ecd81451
ghassenhedfii <3681@holbertonstudents.com>
2023-05-30 13:52:40 +0200

    infinite

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+int _strlen(char *);
+int disgit(char *c);
+void mul(char *s1, char *s2);
+
+#endif /* HOLBERTON_H */
